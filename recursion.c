228003e43b4a50542f1d4fb79885062d25a7cfae
niikiiitaaaa <86907647+niikiiitaaaa@users.noreply.github.com>
2021-07-07 20:31:34 +0530

    prime no.
    check the no. is prime or not

---
+++ recursion.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+ int prime(int, int);
+ int main()
+{
+    int num, ck;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    ck = prime(num, num / 2);
+    if (ck == 1)
+    {
+        printf("%d is a prime number\n", num);
+    }
+    else
+    {
+        printf("%d is not a prime number\n", num);
+    }
+    return 0;
+}
+ 
+int prime(int num, int i)
+{
+    if (i == 1)
+    {
+        return 1;
+    }
+    else
+    {
+       if (num % i == 0)
+       {
+         return 0;
+       }
+       else
+       {
+         return prime(num, i - 1);
+       }       
+    }
+}
